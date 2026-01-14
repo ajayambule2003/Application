5b9611a85ca24cf069cb6ba0f246d49a33182513
Ajay Ambule <ajayambule2003@gmail.com>
2026-01-14 21:58:23 +0530

    updated files;

--- src/main/java/com/service/StudentService.java
+++ src/main/java/com/service/StudentService.java
@@ -1,5 +1,39 @@
 package com.service;
 
-public class StudentService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.model.Student;
+import com.dao.StudentDAO;
+
+@Service
+public class StudentService {
+	private StudentDAO studentDao;
+
+	private void addStudent(Student student) {
+		studentDao.insertStudent(student);
+	}
+
+	private void deleteStudent(int id) {
+		studentDao.removeStudent(id);
+	}
+
+	public void modifyStudent(Student student) {
+		studentDao.updateStudent(student);
+	}
+
+	public List<Student> findAllStudent() {
+		return studentDao.getAllStudets();
+	}
+
+	public StudentDAO getStudentDao() {
+		return studentDao;
+	}
+
+	@Autowired
+	public void setStudentDao(StudentDAO studentDao) {
+		this.studentDao = studentDao;
+	}
 }
