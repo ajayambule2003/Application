72edb66a84ffbbc55a05c0de6cc3cba56879d656
Ajay Ambule <ajayambule2003@gmail.com>
2026-01-15 22:42:50 +0530

    Controller Update

--- src/main/java/com/controller/StudentController.java
+++ src/main/java/com/controller/StudentController.java
@@ -1,9 +1,13 @@
 package com.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.model.Student;
 import com.service.StudentService;
 
 @Controller
@@ -16,6 +20,13 @@ public class StudentController {
 	@GetMapping("/")
 	public String openForm() {
 		return "welcome";
+	}
+	
+	@PostMapping(value="/new", method =RequestMethod.GET)
+	public String studentRegistrationForm(Model model) {
+		model.addAttribute("stud",new Student());
+		return "studform";
+		
 	}
 	
 	
